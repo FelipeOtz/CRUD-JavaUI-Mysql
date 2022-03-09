b5d74b73b907df82860162cf270f33526a09d68e
Felipe <felipeht25@gmail.com>
2022-03-09 19:49:29 -0300

    Colocando Dark Mode

--- src/crud/javaui/mysql/Main.java
+++ src/crud/javaui/mysql/Main.java
@@ -4,12 +4,37 @@
  */
 package crud.javaui.mysql;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author felip
  */
 public class Main {
+
     public static void main(String[] args) {
-        
+
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tela tela = new tela();
+        tela.setVisible(true);
+
+    }
+
+    static void setupQualityHigh(Graphics2D g2d) {
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        // many other RenderingHints KEY/VALUE pairs to specify
     }
 }
